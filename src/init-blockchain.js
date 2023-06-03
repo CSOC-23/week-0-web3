dbff8b3d83cd0676235c8d3a630efab913f440f0
kuv2707 <kislayuv27@gmail.com>
2023-06-03 19:48:51 +0530

    done the assignment

--- src/init-blockchain.js
+++ src/init-blockchain.js
@@ -3,9 +3,14 @@
 const { writeBlockchain } = require("./blockchain-helpers");
 
 const initBlockchain = () => {
+	
 	// Create a genesisBlock and add appropriate hash and previous hash property to genesisBlock
-
+	const genesisBlock={
+		hash: 0,
+		previousHash: null,
+	}
 	// Create a blockchain constant containing genesisBlock
+	const blockchain = [genesisBlock];
 	// Refer writeBlockchain function from blockchain-helpers
 	writeBlockchain(blockchain);
 };
