2ff2d5dc0695093f50a9922c45202e10579f8652
YashSachan2 <122620769+YashSachan2@users.noreply.github.com>
2023-06-13 03:31:16 +0530

    Add files via upload

--- src/add-block.js
+++ src/add-block.js
@@ -4,17 +4,25 @@ const {
 	getTransactions,
 	writeTransactions,
 } = require("./blockchain-helpers");
-
+const crypto = require("crypto-js");
 const addBlock = () => {
 	const blockchain = getBlockchain();
 	const previousBlock = blockchain[blockchain.length - 1];
 	const transactions = getTransactions();
-
+    const newBlock = {
+		hash: generateHash(previousBlock.hash, transactions),
+		previousHash: previousBlock.hash,
+		transactions: transactions,
+	  };
 	// Create a hash, previousHash and transactions property for newBlock and push that into blockchain
 
-	blockchain.push();
+	blockchain.push(newBlock);
 	writeBlockchain(blockchain);
 	writeTransactions([]);
 };
-
+const generateHash = (previousHash, transactions) => {
+	const data = previousHash + JSON.stringify(transactions);
+	const hash = crypto.SHA256(data).toString();
+	return hash;
+  };
 module.exports = { addBlock };
--- src/init-blockchain.js
+++ src/init-blockchain.js
@@ -1,10 +1,16 @@
 // Start here
 
+const { constants } = require("fs/promises");
 const { writeBlockchain } = require("./blockchain-helpers");
 
 const initBlockchain = () => {
+	 const genesisBlock={
+		hash:'0',
+		previousHash:null,
+		transactions:null
+	 }
 	// Create a genesisBlock and add appropriate hash and previous hash property to genesisBlock
-
+      const blockchain=[genesisBlock]
 	// Create a blockchain constant containing genesisBlock
 	// Refer writeBlockchain function from blockchain-helpers
 	writeBlockchain(blockchain);
